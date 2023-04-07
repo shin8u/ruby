a6f99269809555190ae83d1ec331481d5cdcad33
shin8u <98867521+shin8u@users.noreply.github.com>
2023-04-07 04:03:12 +0300

    Update student.rb

--- lb 2/student.rb
+++ lb 2/student.rb
@@ -1,16 +1,96 @@
 class Student
-    attr_accessor :id, :last_name, :first_name, :middle_name, :phone_number, :telegram, :mail, :git
+    attr_reader :id, :last_name, :first_name, :middle_name, :phone_number, :telegram, :mail, :git
 
-    def initialize(last_name:, first_name:, middle_name:, id:nil, phone_number:nil, telegram:nil, mail:nil, git:nil)
+    def initialize(id:nil, last_name:, first_name:, middle_name:, phone_number:nil, telegram:nil, mail:nil, git:nil)
         self.last_name = last_name
         self.first_name = first_name
         self.middle_name = middle_name
         self.id = id
         self.git = git
+        set_contacts(telegram:telegram, phone_number:phone_number, mail:mail)
+        self.git = git
+    end
+
+    def self.is_id?(id)
+        id.match(/^\d+$/)
+    end
+    
+    def self.is_name?(name)
+        name.match(/^[А-Яа-я]+|[A-Za-z]+$/)
+    end
+    
+    def self.is_telegram?(telegram)
+        telegram.match(/^[A-Za-z\d_-]+$/)
+    end
+    
+    def self.is_mail?(mail)
+        mail.match(/^[A-Za-z\d]+@[A-Za-z\d]+\.[\w]+/)
+    end
+    
+    def self.is_git?(git)
+        git.match(/^https:\/\/github\.com\/[A-Za-z\d]+$/) || git.match(/^[A-Za-z\d]+$/)
+    end
+    
+    def self.is_phone_number?(phone_number)
+        phone_number.match(/^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/)
+    end
+
+    def has_git?
+        !git.nil?
+      end
+    
+      def has_contact?
+        !phone.nil? || !telegram.nil? || !mail.nil?
+      end
+    
+      def validate
+        has_git? && has_contact?
+      end
+    
+      def set_contacts(mail:nil, phone_number:nil, telegram:nil)
+        self.mail = mail
         self.phone_number = phone_number
         self.telegram = telegram
-        self.mail = mail
-        self.git = git
+      end
+
+    def id=(last_name)
+        raise ArgumentError unless id.nil? || Student.is_id?(id)
+        @id = id
+      end
+
+    def last_name=(last_name)
+        raise ArgumentError unless last_name.nil? || Student.is_name?(last_name)
+        @last_name = last_name
+      end
+    
+    def first_name=(first_name)
+        raise ArgumentError unless first_name.nil? || Student.is_name?(first_name)
+        @first_name = first_name
+    end
+    
+    def middle_name=(middle_name)
+        raise ArgumentError unless middle_name.nil? || Student.is_name?(middle_name)
+        @middle_name = middle_name
+    end
+    
+    def phone_number=(phone_number)
+        raise ArgumentError unless phone_number.nil? || Student.is_phone_number?(phone_number)
+        @phone_number = phone_number
+    end
+    
+    def telegram=(telegram)
+        raise ArgumentError unless telegram.nil? || Student.is_telegram?(telegram)
+        @telegram = telegram
+    end
+    
+    def mail=(mail)
+        raise ArgumentError unless mail.nil? || Student.is_mail?(mail)
+        @mail = mail
+    end
+    
+    def git=(git)
+        raise ArgumentError unless git.nil? || Student.is_git?(git)
+        @git = git
     end
 
     def to_s
