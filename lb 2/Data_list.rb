c34a6c2a65bcab4e69214a3e9ee908acd3f29c54
shin8u <98867521+shin8u@users.noreply.github.com>
2023-08-15 19:28:53 +0300

    Добавил методы select(number), get_selected, get_names, get_data

--- lb 2/Data_list.rb
+++ lb 2/Data_list.rb
@@ -1,5 +1,26 @@
-class Data_list
-    def initialize(*elements)
-      @data = elements.sort
+class Data_list_2
+    attr_reader :data
+  
+    private_class_method :new
+  
+    def initialize(data)
+      @data = data
+      @selected = []
     end
-end
+  
+    def select(number)
+      @selected << @data[number]
+    end
+  
+    def get_selected
+      @selected.map(&:id)
+    end
+  
+    def get_names
+      raise NotImplementedError
+    end
+  
+    def get_data
+      raise NotImplementedError
+    end
+end 
