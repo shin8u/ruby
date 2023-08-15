dad5f9dde2503e509dfd8c3e9a5c2d7b536ec9db
shin8u <98867521+shin8u@users.noreply.github.com>
2023-08-15 18:47:16 +0300

    Добавил методы возвращающие элемент, кол-во строк и кол-во столбцов

--- lb 2/Data_table.rb
+++ lb 2/Data_table.rb
@@ -2,4 +2,16 @@ class Data_table
     def initialize(data)
       @data = data
     end
+
+    def get_element(row, col)
+        @data[row][col]
+    end
+
+    def get_rows_count
+        @data.size
+    end
+
+    def get_columns_count
+        @data[0].size
+    end
 end
