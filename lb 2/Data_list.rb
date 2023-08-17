89b44643b7d14f9a9e115c4ddd434402258faeb8
shin8u <98867521+shin8u@users.noreply.github.com>
2023-08-17 22:10:05 +0300

    исправлен баг с выбором элементов

--- lb 2/Data_list.rb
+++ lb 2/Data_list.rb
@@ -1,26 +1,25 @@
 class Data_list
-    attr_reader :data
-  
-    private_class_method :new
-  
-    def initialize(data)
-      @data = data
-      @selected = []
-    end
-  
-    def select(number)
-      @selected << @data[number]
-    end
-  
-    def get_selected
-      @selected.map(&:id)
-    end
-  
-    def get_names
-      raise NotImplementedError
-    end
-  
-    def get_data
-      raise NotImplementedError
-    end
+  attr_reader :data, :selected
+
+
+  def initialize(data)
+    @data = data
+    @selected = []
+  end
+
+  def select(number)
+    @selected << data[:data][number]
+  end
+
+  def get_selected
+    @selected.map { |student| student[:id] }
+  end
+
+  def get_names
+    raise NotImplementedError
+  end
+
+  def get_data
+    raise NotImplementedError
+  end
 end 
