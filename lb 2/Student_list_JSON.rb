5aea21022bf484da5c83750c1106b4fe5b2fc073
shin8u <98867521+shin8u@users.noreply.github.com>
2023-08-19 12:47:52 +0300

    реализовал паттерн стратегия

--- lb 2/Student_list_JSON.rb
+++ lb 2/Student_list_JSON.rb
@@ -5,14 +5,12 @@ class Student_list_JSON < Student_list_base
 
   public_class_method :new
 
-  def read_file(file_path)
-    raise ArgumentError, 'Файл не найден' unless File.exist?(file_path)
-    list_hash = JSON.parse(File.read(file_path), { symbolize_names: true })
-    self.students = list_hash.map {|st| Student_short.new(**st)}
+  def list_hash_from_str(str)
+    JSON.parse(str,  {symbolize_names: true })
   end
 
-  def write_to_file(file_path)
-    list_hash = @students.map(&:to_h)
-    File.write(file_path, JSON.generate(list_hash))
+  #получение строки заданного вида из массива хэшей
+  def list_hash_to_str(list_hash)
+    JSON.generate(list_hash)
   end
 end
