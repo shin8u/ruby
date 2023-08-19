cfc04931b1db03d4d7b3d9eb3d5cbe110dcc050e
shin8u <98867521+shin8u@users.noreply.github.com>
2023-08-19 12:47:15 +0300

    реализовал паттерн стратегия

--- lb 2/Student_list_YAML.rb
+++ lb 2/Student_list_YAML.rb
@@ -5,16 +5,12 @@ class Student_list_YAML < Student_list_base
 
   public_class_method :new
 
-  def read_file(file_path)
-    raise ArgumentError, 'Файл не найден' unless File.exist?(file_path)
-    list_hash = YAML.load(File.read(file_path)).map {|hash| hash.transform_keys(&:to_sym)}
-    self.students = list_hash.map {|st| Student_short.new(**st)}
-    # get_new_student_id
+  def list_hash_from_str(str)
+    YAML.load(str).map {|hash| hash.transform_keys(&:to_sym)}
   end
 
-  #запись в файл
-  def write_to_file(file_path)
-    list_hash = @students.map(&:to_h)
-    File.write(file_path, list_hash.map{ |hash| hash.transform_keys(&:to_s)}.to_yaml)
+  #получение строки заданного вида из массива хэшей
+  def list_hash_to_str(list_hash)
+    list_hash.map{ |hash| hash.transform_keys(&:to_s)}.to_yaml
   end
 end
