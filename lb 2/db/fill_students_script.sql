ea79d7c208789826fdd587f7f65b24c06df80fb7
shin8u <98867521+shin8u@users.noreply.github.com>
2023-08-20 21:42:40 +0300

    скрипт добавления элементов в таблицу

---
+++ lb 2/db/fill_students_script.sql
@@ -0,0 +1,8 @@
+use students;
+truncate table student;
+
+insert into student(last_name, first_name, middle_name, phone_number, mail, telegram, git) values
+
+                                                                          ('Иванов', 'Иван', 'Иванович', '+7-936-814-35-98', 'ivan@gmail.com', '@ivan', '@ivan'),
+
+                                                                          ('Иванова', 'Мария', 'Ивановна', '+7-938-767-57-37', 'maria@mail.com', '@maria', '@maria')
