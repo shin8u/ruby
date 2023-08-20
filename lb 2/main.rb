747d79ed75394cc0fd0350f43f2d87b56bec01ea
shin8u <98867521+shin8u@users.noreply.github.com>
2023-08-20 21:27:04 +0300

    подключение к mysql

--- lb 2/main.rb
+++ lb 2/main.rb
@@ -1,5 +1,14 @@
 require_relative 'student'
 require_relative 'student_short'
+require 'mysql2'
+
+@db_host  = "localhost"
+@db_user  = "root"
+@db_pass  = "12345"
+@db_name = "students"
+
+client = Mysql2::Client.new(:host => @db_host, :username => @db_user, :password => @db_pass, :database => @db_name)
+
 
 student1 = Student_short.new(id:1, last_name:'Васильев', first_name:'Олег', middle_name:'Отчество', phone_number:'89184980098', telegram:'oleg', mail:'oleg@mail.ru', git:'OlegVasil')
 student2 = Student_short.new(id:2, last_name:'Нагалевский', first_name:'Артём', middle_name:'Михайлович', phone_number:'+79186768777', telegram:'jakepps', mail:'artem@mail.ru', git:'Jakepps')
