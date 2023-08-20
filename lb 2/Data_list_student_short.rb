af1d7c177838f3747ff1611d4c9478abf78ddab5
shin8u <98867521+shin8u@users.noreply.github.com>
2023-08-21 00:30:38 +0300

    реализация паттерна наблюдателя

--- lb 2/Data_list_student_short.rb
+++ lb 2/Data_list_student_short.rb
@@ -3,6 +3,13 @@ require_relative 'Data_list'
 class Data_list_student_short < Data_list
   attr_reader :column_names, :selected
 
+  attr_accessor :view
+
+  def notify
+    self.view.set_table_params(self.get_objects_attr_names.map{|x| x.to_s}, self.list.size)
+    self.view.set_table_data(self.get_data)
+  end
+  
   def initialize(data)
     super
   end
@@ -16,6 +23,9 @@ class Data_list_student_short < Data_list
   end
 
   def get_data(obj)
-    [obj.git, obj.contact, obj.short_name]
+    table = []
+    counter = 0
+    list.each { |obj| table.append([counter, *get_objects_attr(obj)]); counter += 1}
+    return Data_table.new(table: table)
   end
 end
